1c549b51eafddd719f14e9617299c41eb84c1b32
Buu-Hoang Alix Phuong <buuhoang@IC-CO-IN-SC029.intranet.epfl.ch>
2017-10-02 14:54:45 +0200

    mes modifications , const dans un autre fichier à implanter dans un autre

---
+++ fichier_const.hpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+#ifndef Constante_H
+#include Constante_H
+
+ double const h; // temps par step
+ double const N; // nb de neuron
+  double const Ne = N*0.8;
+  double const Ni = N*0.2;
+  
+ #endif
