292aafe5458dc540901a7813a0a4597d8356c4b1
Alixbuu <32450363+Alixbuu@users.noreply.github.com>
2017-11-03 20:50:30 +0100

    doxy comment modified

--- fichier_const.hpp
+++ fichier_const.hpp
@@ -1,29 +1,33 @@
 #include <iostream>
-using namespace std;
 #include <cmath>
+using namespace std;
 #ifndef Constante_H
 #define Constante_H
 
-  double const h=0.1; // temps par step
-  int const Nbneuron=12500; // nb de neuron
-  int const Ne = ceil(Nbneuron*0.8);// prend le seuil superieur si resultat à  virgules
-  int const Ni = Nbneuron-Ne;
-  double const t_ = 20.00;      //tau
-  double const V_thr = 20;  
-  double const V_reset= 0.0; // reset potential
-  int const trefact=20;
-  double const c1 = exp(-h/t_);
-  double const R=20.00;
-  double const c2 = R*(1-exp(-h/t_));
-  int const delay= 15;
-  double const J=0.1;
-  double const Ji=-0.5;
-  double const Iext=0.0;
-  double const Teta = 20;
-  double const eta = 2;
+/*****************************************
+ * CONSTANTS
+ * 
+ ******************************************/
+  double const h=0.1;//! time per step
+  int const Nbneuron=12500; //! number total of neuron
+  int const Ne = ceil(Nbneuron*0.8);//! number of excitatory neurons
+  int const Ni = Nbneuron-Ne;//!number of inhibitory neurons
+  double const t_ = 20.00;//! Tau
+  double const V_thr = 20;//! V threshold  
+  double const V_reset= 0.0;//! potential reset
+  int const trefact=20;//! time of the refracoty period(step)
+  double const c1 = exp(-h/t_);//! constante of membrane potential
+  double const R=20.00;//! Resistance
+  double const c2 = R*(1-exp(-h/t_));//! constante 2 for the euqation of membrane potential
+  int const delay= 15;//! delay for the buffer
+  double const J=0.1;//! amplitude for excitatory neurons
+  double const Ji=-0.5;//! amplitude for inhibitory neurons
+  double const Iext=0.0;//! external current
+  double const Teta = 20;//! Teta for poisson law
+  double const eta = 2;//! Eta for poisson law
   
   
-  int const t_stopsimu =5000;
+  int const t_stopsimu =1000;//! time of simulation
    // poisson law
   double const Nuthr = (Teta/(0.1*0.8*Nbneuron*J*t_));
   double const Nuext =  (eta*0.1*0.8*Nbneuron*Nuthr);
