372484a7728a225ce2fdb7d29b5587272dfb6104
Alixbuu <32450363+Alixbuu@users.noreply.github.com>
2017-11-07 18:19:59 +0100

    commented the functions

--- network.cpp
+++ network.cpp
@@ -8,36 +8,36 @@
  Nb_excitatory(Ne),
  Nb_Inhibitory(Nbneuron-Ne)
 {
-	for( int i(0); i<Nbneuron;++i)
+	for( int i(0); i<Nbneuron;++i) // create my vector of neurons and call the constructor of neuron
 	{
 		TabNeuron.push_back(Neuron());
 	}
 }
 
 
-void Network::CreateConnexions()
+void Network::CreateConnexions() 
 {
 	for( int i(0); i<Nbneuron; ++i)
 	{
-		vector<int> page;
+		vector<int> page;             // add a  empty vector of connexion for the number of neuron to the table Connexions
 		Connexions.push_back(page);
 	}
 	
 	static random_device rd;
 			static mt19937 gen(rd());
-			static uniform_int_distribution<> connexion_with(0,Nb_excitatory-1);
-			static uniform_int_distribution<> connexions_with(Nb_excitatory,Nbneuron);
+			static uniform_int_distribution<> connexion_with(0,Nb_excitatory-1);  //uniform distribution for excitatory neurons
+			static uniform_int_distribution<> connexions_with(Nb_excitatory,Nbneuron); // uniform distribution for inhibitory neurons
 			
 	for(int i(0); i<Nbneuron;++i)
 	{
-		for( int j(0); j<0.1*Nb_excitatory; ++j)
+		for( int j(0); j<0.1*Nb_excitatory; ++j) //10% of excitatory neurons will have  neuron i in their connexion vector
 		{
 			
 			
 			Connexions[connexion_with(gen)].push_back(i);
 		}
 		
-		for(int n(0); n< 0.1*Nb_Inhibitory; ++n)
+		for(int n(0); n< 0.1*Nb_Inhibitory; ++n)  // 10% of inhibitory neurons will have neuron i in their connexion vector
 		{
 		
 			
@@ -58,26 +58,26 @@ void Network::startsimulation()
 	
 	static random_device rd;
 	static mt19937 gen(rd());
-	static poisson_distribution<> backgroundnoise(lambda);
+	static poisson_distribution<> backgroundnoise(lambda); // poisson distribution for the background noise
  
 	while(t_global<t_stopsimu) {
 		
 		for( size_t i(0); i<TabNeuron.size(); ++i)
 		{
-			if(TabNeuron[i].update(Iext, backgroundnoise(gen)))
-			{  if(t_global>3000)
+			if(TabNeuron[i].update(Iext, backgroundnoise(gen))) //update all the neuron 
+			{  if(t_global>3000)                                   // write in the file only from the 3000 steps so the neurons have time to desynchronize themsleve-> plot better
 				{
 				spikeC<<t_global<<'\t'<<i+1<<'\n';
 			    }
 				for( size_t j(0); j<Connexions[i].size(); ++j)
 				{
-					int connect=Connexions[i][j];
+					int connect=Connexions[i][j]; //the indice in the TabNeuron of the neuron to connect
 					
 					if( i<Nb_excitatory-1)
 					{
-						TabNeuron[i].sendspike(TabNeuron[connect],J,t_global);	
+						TabNeuron[i].sendspike(TabNeuron[connect],J,t_global);	// if the neuron is excitatory send a positive spike to its connexions 
 					}else{
-						TabNeuron[i].sendspike(TabNeuron[connect],Ji,t_global);	
+						TabNeuron[i].sendspike(TabNeuron[connect],Ji,t_global);	//if the neuron is inhbithory send a positive spike to its connexions
 					}
 				}	
 			}	
